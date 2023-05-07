015be4f008f56e7b244aa17a0704db22aa0f3fc2
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-05-06 17:46:52 -0700

    added functions to update display with test object

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -63,6 +63,91 @@
 //Debug Printer
 function p(me){console.log(me);}
 
+p(typeof(dayjs().format('M/DD/YYYY')))
+
+//#region Variable declaration
+let weatherObj = {
+    "date": "",
+    "temperature": "",  //def: Kelvin; metric: C; imp: F
+    "wind": "",         //def: metre/sec; metric: metre/sec; imp: miles/hour //.wind_speed
+    "humidity": "",     //display as %
+    "icon": "",         //weather.icon
+}
+let cityStr = "";
+let forecastArr=[]
+
+//#endregion 
+
+//#region Placeholder
+let testObj = {
+    "date": dayjs().format('M/DD/YYYY'),
+    "temperature": 282.21,
+    "wind": 8.75,
+    "humidity": 65,
+    "icon": "03d", //https://openweathermap.org/img/wn/03d@2x.png
+}
+let testerArr=[];
+for (i=0;i<=5;i++){
+    testerArr.push(testObj);
+}
+//#endregion
 
 
-//!end
+//#region General HTML functions
+
+//#endregion
+
+//#region Search //TODO(2)
+
+// recieve input from user
+    // geting city
+    // what kind of metric used
+// check api for valid
+
+
+
+
+//! ready document before call api
+
+//#endregion
+
+//#region Display //TODO(1)
+let display = $("#display");p(display);
+//let currentCard= display.children().eq(0)
+
+function updateCardData(cardObj, dataObj){
+    let cardDataList = cardObj.children();  // 
+    cardDataList.eq(0).text(dataObj.date);
+    cardDataList.eq(1).text(dataObj.temperature);
+    cardDataList.eq(2).text(dataObj.wind);
+    cardDataList.eq(3).text(dataObj.humidity);
+    p(cardDataList);
+} // 
+
+function updateForecast(){
+    let currentCard ="";
+    for (i=0; i<= 5; i++){
+        currentCard = display.children().eq(i);
+        p(currentCard);
+        updateCardData(currentCard, testerArr[i]); 
+    }
+} //TODO: change from testerArr to actual
+
+
+updateForecast();
+
+//updateData(currentCard,testObj);
+
+
+//updateCity
+
+
+
+
+//function get(){}
+
+
+
+//#endregion
+
+// !end of File
